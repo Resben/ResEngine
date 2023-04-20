33e9de018b6590fd34a663e2ac52663041a8204c
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2023-04-21 04:15:58 +0800

    [Docs] added comments

--- AEngine/src/Platform/ReactPhysics3D/ReactRaycaster.h
+++ AEngine/src/Platform/ReactPhysics3D/ReactRaycaster.h
@@ -5,22 +5,31 @@
 
 namespace AEngine
 {
+	/**
+		* @class ReactRaycaster
+		* @brief Implementation for the Raycaster class
+		* @author Lane O'Rafferty (33534304)
+	**/
 	class ReactRaycaster : public Raycaster, public rp3d::RaycastCallback
 	{
 	public:
 		ReactRaycaster(PhysicsWorld& world);
 
 		virtual rp3d::decimal notifyRaycastHit(const rp3d::RaycastInfo& info) override;
-
+		
 		virtual bool CastRay(Math::vec3 rayStart, Math::vec3 rayEnd) override;
 
 		virtual const RayCastInfo& GetInfo() const override;
 
 	private:
+
+		// the physics world
 		PhysicsWorld& _world;
 
+		// the info from the raycast hit
 		RayCastInfo _info;
 
+		// the hit success of the raycast
 		bool _isHit;
 	};
 }
