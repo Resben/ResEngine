48bca11d3027eae6a6e3030a117a7b827d554e20
CaptCandy <102133867+CaptCandy@users.noreply.github.com>
2023-04-21 14:46:07 +0800

    [docx] Adding doxy comments to script and scriptState

--- AEngine/src/AEngine/Script/Script.h
+++ AEngine/src/AEngine/Script/Script.h
@@ -9,15 +9,40 @@ namespace AEngine
 {
 	class Entity;
 
+		/**
+		 * @class Scene
+		 * @brief Script Class for the script 
+		 * @author Geoff Candy (34183006)
+		**/
 	class Script : public Asset
 	{
 	public:
+		
+		/**
+		 * @brief Script Constructor for the Script
+		 * @param std::string, std::string
+		 **/
 		Script(const std::string& ident, const std::string& fname);
 
+		/**
+		 * @brief OnUpdate Method called from within lua scripts
+		 **/
 		void OnStart();
+
+		/**
+		 * @brief OnUpdate Method called from within lua scripts
+		 * @param float, Entity
+		 **/
 		void OnUpdate(float deltaTime, Entity entity);
+		
+		/**
+		 * @brief OnDestroy Method called from within lua scripts
+		 **/
 		void OnDestroy();
 		
+		/**
+		 * @brief Create Method to create the Script pointer used in Components 
+		 **/
 		static std::shared_ptr<Script> Create(const std::string& ident, const std::string& fname);
 	
 	private:
