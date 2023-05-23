232e3e9f7f905fe8e21535e26d9d50bff9b0a09d
Christien Alden <christien.alden@zoho.com>
2023-05-23 14:05:14 +0800

    [Docs] MouseEvent

--- AEngine/src/AEngine/Events/MouseEvent.h
+++ AEngine/src/AEngine/Events/MouseEvent.h
@@ -1,32 +1,31 @@
 /**
- * @file
- * @author Christien Alden (34119981)
- * @author Ben Hawkins (34112619)
-**/
+ * \file
+ * \author Christien Alden (34119981)
+ * \author Ben Hawkins (34112619)
+*/
 #pragma once
-#include "Event.h"
 #include "AEngine/Input/KeyCodes.h"
+#include "Event.h"
 
 namespace AEngine
 {
 		/**
-		 * @class MouseMoved
-		 * @brief Created when the cursor is moved in the application window
-		**/
+		 * \class MouseMoved
+		 * \brief Created when the cursor is moved in the application window
+		*/
 	class MouseMoved : public Event
 	{
 	public:
 			/**
-			 * \brief Construct a new Mouse Moved event
 			 * \param[in] pos The new cursor position
-			 */
+			*/
 		MouseMoved(Math::vec2 pos)
-			: m_pos(pos) {}
+			: m_pos{ pos } {}
 
 			/**
 			 * \brief Get the new cursor position
 			 * \return The new cursor position
-			 */
+			*/
 		Math::vec2 GetPos() const { return m_pos; }
 		AE_EVENT_META(Window, MouseMoved)
 
@@ -35,23 +34,22 @@ namespace AEngine
 	};
 
 		/**
-		 * @class MouseButtonPressed
-		 * @brief Created when a mouse button has been pressed
-		**/
+		 * \class MouseButtonPressed
+		 * \brief Created when a mouse button has been pressed
+		*/
 	class MouseButtonPressed : public Event
 	{
 	public:
 			/**
-			 * \brief Construct a new Mouse Button Pressed event
 			 * \param[in] button The mouse button that was pressed
-			 */
+			*/
 		MouseButtonPressed(const AEMouse button)
-			: m_mouseButton(button) {}
+			: m_mouseButton{ button } {}
 
 			/**
 			 * \brief Get the mouse button that was pressed
 			 * \return The mouse button that was pressed
-			 */
+			*/
 		AEMouse GetButton() { return m_mouseButton; }
 		AE_EVENT_META(Window, MouseButtonPressed)
 
@@ -60,23 +58,22 @@ namespace AEngine
 	};
 
 		/**
-		 * @class MouseButtonReleased
-		 * @brief Created when a mouse button has been released
-		**/
+		 * \class MouseButtonReleased
+		 * \brief Created when a mouse button has been released
+		*/
 	class MouseButtonReleased : public Event
 	{
 	public:
 			/**
-			 * \brief Construct a new Mouse Button Released event
 			 * \param[in] button The mouse button that was released
-			 */
+			*/
 		MouseButtonReleased(const AEMouse button)
-			: m_mouseButton(button) {}
+			: m_mouseButton{ button } {}
 
 			/**
 			 * \brief Get the mouse button that was released
 			 * \return The mouse button that was released
-			 */
+			*/
 		AEMouse GetButton() { return m_mouseButton; }
 		AE_EVENT_META(Window, MouseButtonReleased)
 
@@ -85,23 +82,22 @@ namespace AEngine
 	};
 
 		/**
-		 * @class MouseScrolled
-		 * @brief Created when the mouse wheel is scrolled
-		**/
+		 * \class MouseScrolled
+		 * \brief Created when the mouse wheel is scrolled
+		*/
 	class MouseScrolled : public Event
 	{
 	public:
 			/**
-			 * \brief Construct a new Mouse Scrolled event
 			 * \param[in] scroll The amount the mouse wheel was scrolled
-			 */
+			*/
 		MouseScrolled(Math::vec2 scroll)
-			: m_scroll(scroll) {}
+			: m_scroll{ scroll } {}
 
 			/**
 			 * \brief Get the amount the mouse wheel was scrolled
 			 * \return The amount the mouse wheel was scrolled
-			 */
+			*/
 		Math::vec2 GetScroll() { return m_scroll; }
 		AE_EVENT_META(Window, MouseScrolled)
 
