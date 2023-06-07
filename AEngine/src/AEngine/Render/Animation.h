86228c2dddbc54f8ba5f270088c5e49d5c0be2ba
Resben <bendavidhawkins@gmail.com>
2023-06-07 17:35:33 +0800

    [Docs] animation documentation + tabbing

--- AEngine/src/AEngine/Render/Animation.h
+++ AEngine/src/AEngine/Render/Animation.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @author Ben Hawkins (34112619)
+**/
+
 #pragma once
 
 #include <vector>
@@ -16,56 +21,123 @@
 namespace AEngine
 {
 
-    struct AnimationData
-    {
-        float duration;
-        int ticksPerSecond;
-        float currentTime;
-        float lastTime = -1.0f;
-        std::vector<Bone> bones;
-    };
+		/**
+		 * @struct AnimationData
+		 * @brief Stores a single animation
+		**/
+	struct AnimationData
+	{
+		float duration;
+		int ticksPerSecond;
+		float currentTime;
+		float lastTime = -1.0f;
+		std::vector<Bone> bones;
+	};
 
-    struct BoneInfo
-    {
-        int id;
-        Math::mat4 offset;
-    };
+		/**
+		 * @struct BoneInfo
+		 * @brief Stores a bone ID and local offset
+		**/
+	struct BoneInfo
+	{
+		int id;
+		Math::mat4 offset;
+	};
 
-    struct SceneNode
-    {
-        Math::mat4 transformation;
-        std::string name;
-        int numChildren;
-        std::vector<SceneNode> children;
-    };
+		/**
+		 * @struct SceneNode
+		 * @brief Stores Assimp node data
+		**/
+	struct SceneNode
+	{
+		Math::mat4 transformation;
+		std::string name;
+		int numChildren;
+		std::vector<SceneNode> children;
+	};
 
-    class Animation
-    {
-    public:
-        ~Animation();
+		/**
+		 * @class Animation
+		 * @brief Contains all of the animations of a model
+		**/
+	class Animation
+	{
+	public:
+			/// @brief Deconstructor
+		~Animation();
+			/// @brief Constructor
 		Animation();
 
-        void Load(const aiScene* scene);
-        void Clear();
+			/**
+			 * @brief Load a animation from a aiScene
+			 * @param[in] scene Assimp aiScene object
+			 * 
+			 * @todo Remove testing code
+			**/
+		void Load(const aiScene* scene);
+			/**
+			 * @brief Clear animation data
+			**/
+		void Clear();
+			/**
+			 * @brief Set a animation to play
+			 * @param[in] id name of animation
+			**/
+		void SetAnimation(std::string id);
+			/**
+			 * @brief Update an animation
+			 * @param[in] dt Delta time
+			**/
+		void UpdateAnimation(float dt);
+			/**
+			 * @brief Get bone transforms (up to 100 bones)
+			 * @return vector of transform matrices
+			 * 
+			 * @note Must call UpdateAnimation beforehand
+			**/
+		std::vector<Math::mat4>& GetFinalBoneMatrices();
 
-        void SetAnimation(std::string id);
-        void UpdateAnimation(float dt);
-        std::vector<Math::mat4>& GetFinalBoneMatrices();
+	private:
 
-    private:
+			/**
+			 * @brief Map bones to name
+			 * @param[in] scene Assimp aiScene object
+			**/
+		void MapBones(const aiScene* scene);
+			/**
+			 * @brief Load Assimp aiNode structure from root node into SceneNode structure recursively
+			 * @param[in] node SceneNode to load too
+			 * @param[in] src aiNode to load from
+			**/
+		void ProcessNode(SceneNode& node, const aiNode* src);
+			/**
+			 * @brief Load one animation from Assimp structures
+			 * @param[in] animation Assimp aiAnimation object
+			**/
+		void LoadAnimation(const aiAnimation* animation);
+			/**
+			 * @brief Calculate all bone transformations recursively
+			 * @param[in] node SceneNode
+			 * @param[in] parentTransform transform of parent SceneNode
+			**/
+		void CalculateBoneTransform(const SceneNode* node, Math::mat4 parentTransform);
+			/**
+			 * @brief Get a Bone object from vector of current animation
+			 * @param[in] name string name of bone
+			**/
+		Bone* GetBone(const std::string& name);
 
-        void MapBones(const aiScene* scene);
-        void ProcessNode(SceneNode& dest, const aiNode* src);
-        void LoadAnimation(const aiAnimation* animation);
-
-        void CalculateBoneTransform(const SceneNode* node, Math::mat4 parentTransform);
-        Bone* GetBone(const std::string& name);
-
-        AnimationData m_currentAnimation;
-        bool isAnimated;
-        std::vector<Math::mat4> m_FinalBoneMatrices;
-        SceneNode m_RootNode;
-        std::map<std::string, BoneInfo> m_BoneInfoMap;
-        std::map<std::string, AnimationData> m_animations;
-    };
+			/// @brief Current animation selected
+		AnimationData m_currentAnimation;
+			/// @brief Is model animated
+		bool isAnimated;
+			/// @brief Vector of bone matrices
+		std::vector<Math::mat4> m_FinalBoneMatrices;
+			/// @brief Root node of SceneNode data
+		SceneNode m_RootNode;
+			/// @brief Map bone name to info
+		std::map<std::string, BoneInfo> m_BoneInfoMap;
+			/// @brief Vector of all animations
+		std::map<std::string, AnimationData> m_animations;
+	};
 }
--- AEngine/src/AEngine/Render/Bone.h
+++ AEngine/src/AEngine/Render/Bone.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * @author Ben Hawkins (34112619)
+**/
+
 #pragma once
 
 #include <vector>
@@ -12,46 +17,99 @@
 
 namespace AEngine
 {
-    struct KeyPosition
-    {
-        Math::vec3 position;
-        float timeStamp;
-    };
+		/**
+		 * @struct KeyPosition
+		 * @brief Stores a position keyframe
+		**/
+	struct KeyPosition
+	{
+		Math::vec3 position;
+		float timeStamp;
+	};
 
-    struct KeyRotation
-    {
-        Math::quat rotation;
-        float timeStamp;
-    };
+		/**
+		 * @struct KeyRotation
+		 * @brief Stores a rotation keyframe
+		**/
+	struct KeyRotation
+	{
+		Math::quat rotation;
+		float timeStamp;
+	};
 
-    struct KeyScale
-    {
-        Math::vec3 scale;
-        float timeStamp;
-    };
+		/**
+		 * @struct KeyScale
+		 * @brief Stores a scale keyframe
+		**/
+	struct KeyScale
+	{
+		Math::vec3 scale;
+		float timeStamp;
+	};
 
-    class Bone
-    {
-    public:
+		/**
+		 * @struct Bone
+		 * @brief Stores animation data for a Bone
+		**/
+	class Bone
+	{
+	public:
+			/**
+			 * @brief Contructor
+			 * @param[in] animNode Assimp aiNodeAnim object
+			**/
+		Bone(const aiNodeAnim* animNode);
+			/**
+			 * @brief Update and return a bone transform
+			 * @param[in] animationTime current time in animation
+			**/
+		Math::mat4 GetLocalTransform(float animationTime);
+			/**
+			 * @brief Return a bone name
+			 * @return String bone name
+			**/
+		std::string GetBoneName() const;
 
-        Bone(const aiNodeAnim* animNode);
-        Math::mat4 GetLocalTransform(float animationTime);
-        std::string GetBoneName() const;
+	private:
+			/**
+			 * @brief Load bone keyframes
+			 * @param[in] animNode Assimp animNode object
+			**/
+		void LoadData(const aiNodeAnim* animNode);
+			/**
+			 * @brief Calculates the scale between two timestamps
+			 * @param[in] currentTimeStamp first keyframe timestamp
+			 * @param[in] nextTimeStamp next keyframe timestamp
+			 * @param[in] animationTime time of animation
+			**/
+		float GetScaleFactor(float currentTimeStamp, float nextTimeStamp, float animationTime);
+			/**
+			 * @brief Interpolate a postion between two keyframes
+			 * @param[in] animationTime time of animation
+			 * @return position transform
+			**/
+		Math::mat4 InterpolatePosition(float animationTime);
+			/**
+			 * @brief Interpolate a rotation between two keyframes
+			 * @param[in] animationTime time of animation
+			 * @return rotation transform
+			**/
+		Math::mat4 InterpolateRotation(float animationTime);
+			/**
+			 * @brief Interpolate a scale between two keyframes
+			 * @param[in] animationTime time of animation
+			 * @return scale transform
+			**/
+		Math::mat4 Bone::InterpolateScaling(float animationTime);
 
-    private:
+			/// @brief vector of position keyframes
+		std::vector<KeyPosition> m_positions;
+			/// @brief vector of rotation keyframes
+		std::vector<KeyRotation> m_rotations;
+			/// @brief vector of scale keyframes
+		std::vector<KeyScale> m_scales;
+			/// @brief bone name
+		std::string m_name;
 
-        void LoadData(const aiNodeAnim* animNode);
-        
-        float GetScaleFactor(float lastTimeStamp, float nextTimeStamp, float animationTime);
-        Math::mat4 InterpolatePosition(float animationTime);
-        Math::mat4 InterpolateRotation(float animationTime);
-        Math::mat4 Bone::InterpolateScaling(float animationTime);
-
-        std::vector<KeyPosition> m_positions;
-        std::vector<KeyRotation> m_rotations;
-        std::vector<KeyScale> m_scales;
-
-        std::string m_name;
-
-    };
+	};
 }
