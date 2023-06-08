9ba1d9fa8f13d49455ba940d9416643b607baf92
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2023-06-08 18:24:35 +0800

    [WIP] removed debug alerts from testing

--- AEngine/src/AEngine/Physics/PlayerController.cpp
+++ AEngine/src/AEngine/Physics/PlayerController.cpp
@@ -73,7 +73,6 @@ namespace AEngine
 		{
 			if (!m_inGroundedState)
 			{
-				std::cout << "Grounded" << std::endl;
 				m_body->SetDrag(m_properties.moveDrag);
 				m_body->SetVelocity({ m_currentDirection.x, 0, m_currentDirection.z });
 				m_fallingSpeed = 0.0f;
@@ -98,7 +97,6 @@ namespace AEngine
 		{
 			if (!m_inFallingState)
 			{
-				std::cout << "Falling" << std::endl;
 				m_body->SetDrag(m_properties.fallDrag);
 				m_inFallingState = true;
 				m_inGroundedState = false;
