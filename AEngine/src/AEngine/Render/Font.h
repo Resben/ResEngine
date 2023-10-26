d600d9890015a4a9edb4d51793b6b7d2451c8e88
Resben <bendavidhawkins@gmail.com>
2023-10-27 00:40:32 +0800

    [Docs] UI documentation

--- AEngine/src/AEngine/Render/Font.h
+++ AEngine/src/AEngine/Render/Font.h
@@ -22,8 +22,7 @@ namespace AEngine
 
 		/**
 		 * @class Font
-		 * @brief Stores a map of font data, renders a
-		 * text to screen
+		 * @brief Stores a map of font data, renders a text to screen
 		 * @author Ben Hawkins
 		**/
 	class Font : public Asset
@@ -31,23 +30,28 @@ namespace AEngine
 	public:
 			/**
 			 * @brief Constructor to create font
-			 * @param[in] map<char, Character> of font data
+			 * @param[in] ident : identifier of font
+			 * @param[in] path : path to font file
 			**/
 		Font(const std::string& ident, const std::string& path);
 			
 		~Font();
 
-        void Load(const std::string& path);
+			/**
+			 * @brief Loads a font from a file
+			 * @param[in] path : path to font file
+			 * @retval void
+			**/
+		void Load(const std::string& path);
 
 			/**
 			 * @brief Renders a string on a screen (credit Learnopengl refer to header)
-			 * @param[in] s : Shader to use for rendering 
-			 * @param[in] test : String of text to render
-			 * @param[in] pos : vec2 float position on screen (0,0 at bottom left)
-			 * @param[in] scale : float scale of text
-			 * @param[in] color : vec3 float colour of text
-			 * @param[in] width : float width of screen
-			 * @param[in] height : float height of screen
+			 * @param[in] billboard : Should UI be a billboard 
+			 * @param[in] screenspace : true if camera screenspace false if worldspace
+			 * @param[in] camera : active camera
+			 * @param[in] text : text to render
+			 * @param[in] transform : transform of text
+			 * @param[in] colour : colour of text
 			 * @retval void
 			**/
 		void Render(bool billboard, bool screenspace, const PerspectiveCamera* camera, std::string text, Math::mat4 transform, Math::vec4 colour);
@@ -62,10 +66,7 @@ namespace AEngine
 		void GenerateFont();
 
 		unsigned int m_vbo, m_vao;
-
-			/// map of char to a font character
 		std::map<char, Character> m_fontData;
-
 		static SharedPtr<Shader> s_textShader;
 	};
 }
